d80b950995b515c4b1fa8c4322b7737b3eca8355
JJungH0 <junghwan__@naver.com>
2026-09-10 09:27:25 +0900

    [INIT]: 프로젝트 초기 설정

---
+++ user-service/src/main/java/com/shopflow/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
